f224dccc76de9609d54337af53778bad61a7f7cd
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-07 13:28:37 -0300

    aprendendo a importar

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const ObjJavascript = require('./literals/objeto') // arquivo principal é js então não preciso avisar que esse é js.
+
+console.log(ObjJavascript.name)
+
---
+++ src/literals/objeto.js
@@ -0,0 +1,11 @@
+const Produto = {
+    id: 1,
+    name: "Iphone 12",
+    price: 8000,
+    discount: 5,
+    description: {
+        features: "touch screen"
+    }
+}
+
+module.exports = Produto
