50c42c4ba9a48c5880b6bab59e494a3e6b155f52
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-07 14:17:42 -0300

    preferencia por trabalhar no formato de classe

--- src/class/anatomia.js
+++ src/class/anatomia.js
@@ -14,6 +14,15 @@ class Person {
     }
 }
 
+// Sobre este método abaixo (NewPerson) - não é recomendado.
+// Era feito dessa forma antes do javascript dar suporte às classes
+// É preferível trabalhar no formato de classe.
+function NewPerson(name) {
+    this._name = name;
+}
+const SegundaPessoa = new NewPerson('Maricotinha')
+
 const Pessoa = new Person;
 
-module.exports = Pessoa;
+// module.exports = Pessoa;
+module.exports = SegundaPessoa;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const ObjJavascript = require('./literals/objeto') // arquivo principal é js então não preciso avisar que esse é js.
 const ObjJSON = require('./literals/objeto.json')
+// const Pessoa = require('./class/anatomia')
+const SegundaPessoa = require('./class/anatomia')
 
-console.log(ObjJavascript.name)
-console.log(ObjJavascript.getDiscount())
-console.log(ObjJavascript.getFinalPrice())
+// console.log(ObjJavascript.name)
+// console.log(ObjJavascript.getDiscount())
+// console.log(ObjJavascript.getFinalPrice())
 
+// console.log(ObjJSON.produto[1].name)
+// console.log(Pessoa.hello('Amauri', 'dev', 27))
 
-console.log(ObjJSON.produto[1].name)
+console.log(SegundaPessoa._name)
